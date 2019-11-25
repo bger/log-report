09817f38698a0d6bfaa34f77fe8faae4874df5d1
borya <boris.gerlovsky@gmail.com>
2019-11-25 11:29:01 +0300

    [Printer] Add spec for printer

---
+++ spec/lib/printer_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe 'Printer' do
+  subject(:printer) { described_class.new(report: report) }
+
+  describe '#display' do
+    it 'prints pages ordered by total views' do
+      skip
+    end
+
+    it 'prints pages ordered by unique views' do
+      skip
+    end
+  end
+end
